161717705b8faa8c4ad472f8f7722d041101f93a
anderson <andersonsenafilho@gmail.com>
2023-10-09 21:21:45 -0300

    meu commit

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -4,6 +4,6 @@ from .views import ImovelList, ImovelDetail, page_list
 urlpatterns = [
     path('imoveis/', ImovelList.as_view(), name='imovel-list'),
     path('imoveis/<int:pk>/', ImovelDetail.as_view(), name='imovel-detail'),
-    path('imoveis-list/', page_list, name='imoveis-list'),  # Adicione esta linha
+    path('', page_list, name='imoveis-list'),  # Adicione esta linha
 ]   
 
